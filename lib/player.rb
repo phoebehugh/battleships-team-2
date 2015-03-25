3795319ba204b2297c52bd299886d6887ac42d99
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-25 12:13:42 +0000

    adds the ability to shoot a coordinate on the board

--- lib/player.rb
+++ lib/player.rb
@@ -1,21 +1,20 @@
 class Player
 
-  attr_reader :name, :own_board
+  attr_reader :name, :own_board, :other_board
   attr_writer :name
 
-  def initialize name, board
+  def initialize name, own_board, other_board
     @name = name
-    @own_board = board
+    @own_board = own_board
+    @other_board = other_board
   end
 
   def place_ship(ship, coordinate)
     own_board.place_ship(ship, coordinate)
   end
 
-end
+  def hit(coordinate)
+    other_board.hit(coordinate)
+  end 
 
-class Board
-  def place_ship(ship_type, at_coordinate)
-    puts "ship placed at #{at_coordinate}"
-  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,21 +1,23 @@
 require 'player'
 
 describe Player do
-  let(:board) { double :board }
-  let(:player) { Player.new "Phoebe", board }
+  let(:own_board) { double :own_board }
+  let(:other_board) { double :other_board }
+  let(:player) { Player.new "Player", own_board, other_board }
   let(:ship) { double :ship }
 
   it "has a name" do
-    expect(player.name).to eq "Phoebe"
+    expect(player.name).to eq "Player"
   end
 
   it "can place ships on the board at a coordinate" do
-    expect(board).to receive(:place_ship).with(ship, :A1)
+    expect(own_board).to receive(:place_ship).with(ship, :A1)
     player.place_ship(ship, :A1) 
   end
 
-  xit "can shoot at a board at a coordinate" do
-    
+  it "can shoot at a board at a coordinate" do
+    expect(other_board).to receive(:hit).with(:A1)
+    player.hit(:A1)
   end
 
   xit "knows if he has floating ships" do
