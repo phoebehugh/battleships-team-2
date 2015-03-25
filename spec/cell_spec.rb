438fa33527777103b6d87f33b2ce003fdf4cc7f6
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-25 09:58:26 +0000

    Updates frigate to ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,10 +4,10 @@ describe Cell do
 
   let(:cell) { Cell.new }
   let(:cell_with_content) { Cell.new }
-  let(:frigate) { double :frigate }
+  let(:ship) { double :ship }
 
   before do 
-    cell_with_content.content = frigate
+    cell_with_content.content = ship
   end
 
   it 'is not shot when created' do
@@ -16,8 +16,8 @@ describe Cell do
   end
 
   it 'can have content' do
-    cell.content = frigate
-    expect(cell.content).to eq frigate
+    cell.content = ship
+    expect(cell.content).to eq ship
   end
 
   it 'can be shot at' do
@@ -25,18 +25,18 @@ describe Cell do
   end
 
   it 'shoots at its content when shot at' do
-    expect(frigate).to receive :hit!
+    expect(ship).to receive :hit!
     cell_with_content.receive_shot
   end
 
   it 'knows that it was shot at' do
-    allow(frigate).to receive(:hit!)
+    allow(ship).to receive(:hit!)
     cell_with_content.receive_shot
     expect(cell_with_content).to be_shot
   end
 
   it 'cannot be shot twice' do
-    allow(frigate).to receive :hit!
+    allow(ship).to receive :hit!
     cell_with_content.receive_shot
     expect { cell_with_content.receive_shot }.to raise_error 'Already been shot'
   end
---
+++ spec/player_spec.rb
@@ -0,0 +1,26 @@
+require 'player'
+
+describe Player do
+  let(:player) { Player.new }
+
+  it "has a name" do
+    expect(player).to respond_to :name
+  end
+
+  it "can place ships on the board" do
+    
+  end
+
+  it "can shoot at a board at a coordinate" do
+    
+  end
+
+  it "knows if he has floating ships" do
+    
+  end
+
+  it "knows if he has won or lost" do
+
+  end
+
+end
