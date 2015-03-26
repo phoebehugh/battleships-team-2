54934fad24d98710dc33e2b728cf52305575d153
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-26 15:37:57 +0000

    Updates player

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,24 @@
+require_relative 'board'
+require_relative 'cell'
+require_relative 'ship'
+require_relative 'water'
+
 class Player
 
-attr_accessor :name, :own_board
+  attr_reader :name, :own_board
+  attr_writer :name
 
   def initialize name, own_board
     @name = name
     @own_board = own_board
   end
 
-  def board
+  def place_ship(ship, coordinate)
+    own_board.place_ship(ship, coordinate)
+  end
+
+  def receive_shot(coordinate)
+    own_board.receive_shot(coordinate)
   end
 
 end
